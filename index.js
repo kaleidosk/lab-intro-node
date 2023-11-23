10bf806e4f720bc429ef3c142cc4ba133457f4d1
Jennifer Manella <jennifermanella@MacBook-Pro-de-Jennifer.local>
2023-11-23 10:31:08 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=> {
+      if (a>b) {
+        return 1;
+      }else if (a<b){
+        return -1;
+      }else {
+        return 0;
+      }
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos>this.items.length-1) {
+      throw new Error ("OutOfBounds")
+    }else{
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error ("EmptySortedList")
+    }else{
+      return this.items [this.length-1]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error ("EmptySortedList")
+    }else{
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0){
+      return 0
+    }else{
+    return this.items.reduce((total, currentValue) =>{
+      return total + currentValue;
+    }, 0)}
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0){
+      throw newError ("EmptySortedList")
+    }else{
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
